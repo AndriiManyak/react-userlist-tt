f95144d8c100492302a67747dba655c6f788e971
VoduDay <andriy.maniak@gmail.com>
2020-11-22 19:36:07 +0200

    create User component and add basic style

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-export const User = ({ name, surname }) => (
-  <div>
-    <p>{`${name} ${surname}`}</p>
+export const User = ({ name, surname, desc }) => (
+  <div className="User">
+    <h3 className="User__name">{`${name} ${surname}`}</h3>
+    <p className="User__description">{desc}</p>
   </div>
 );

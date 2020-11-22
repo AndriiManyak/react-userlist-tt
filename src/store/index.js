bd7884d5be00af841b432a1c6299fb34665befe0
VoduDay <andriy.maniak@gmail.com>
2020-11-23 00:30:49 +0200

    fixed store selectors

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,8 @@
-/* eslint-disable no-console */
 import { createStore, combineReducers } from 'redux';
 
 import usersReducer from './users';
 
 export function getUsers(state) {
-  console.log(state.usersState.users);
-
   return state.usersState.users;
 }
 

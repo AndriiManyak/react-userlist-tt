237b37c42de179eab5afafa5132afe0261f42304
VoduDay <andriy.maniak@gmail.com>
2020-11-22 21:37:42 +0200

    add basic style for Users component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,15 +1,21 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import request from '../../api/api';
 import { getUsers } from '../../store';
 import { actions } from '../../store/users';
+import { Pagination } from '../Pagination/Pagination';
 
 import { UserList } from '../UserList';
+import './Users.scss';
 
 export const Users = () => {
   const users = useSelector(getUsers);
   const dispatch = useDispatch();
   const [randomIndex, setRandomIndex] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(5);
 
   useEffect(() => {
     getUsersFromServer();
@@ -32,13 +38,38 @@ export const Users = () => {
     Math.floor(Math.random() * (max - min + 1)) + min
   );
 
+  const changePage = useCallback((pageNumber) => {
+    setCurrentPage(pageNumber);
+  }, []);
+
+  const selectNextPage = useCallback(() => {
+    setCurrentPage(prevState => prevState + 1);
+  }, []);
+
+  const selectPreviousPage = useCallback(() => {
+    setCurrentPage(prevState => prevState - 1);
+  }, []);
+
   return (
-    <div>
+    <div className="Users">
       {users && (
-        <UserList
-          users={users}
-          randomIndex={randomIndex}
-        />
+        <>
+          <UserList
+            users={users}
+            randomIndex={randomIndex}
+            currentPage={currentPage}
+            usersPerPage={usersPerPage}
+          />
+
+          <Pagination
+            totalElements={users.length}
+            elementsPerPage={usersPerPage}
+            currentPage={currentPage}
+            changePage={changePage}
+            selectNextPage={selectNextPage}
+            selectPreviousPage={selectPreviousPage}
+          />
+        </>
       )}
     </div>
   );
